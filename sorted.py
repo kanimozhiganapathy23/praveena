b466c5d1ed7ccc734f05f29d3163c8c23ad08e1b
Nagalakshmikesavan <34570264+Nagalakshmikesavan@users.noreply.github.com>
2018-06-12 09:28:41 +0530

    Create sorted.py

---
+++ sorted.py
@@ -0,0 +1,8 @@
+a=int(raw_input())
+list=[]
+for n in range(0,a):
+            int_input=raw_input()
+            list.append(int_input)
+for i in list:
+            sorted(list)
+print(sorted(list))     
