ba7cfb26b9e676faec4100028467aa5efb91b80d
Nagalakshmikesavan <34570264+Nagalakshmikesavan@users.noreply.github.com>
2018-06-12 10:48:07 +0530

    Update median.py

--- median.py
+++ median.py
@@ -1,8 +1,8 @@
 a=int(raw_input())
 list=[]
 for n in range(0,a):
-            int_input=int(raw_input())
-            list.append(int_input)
+            i=int(raw_input())
+            list.append(i)
 a=len(list)/2
 print(a)
 print(list[a])
