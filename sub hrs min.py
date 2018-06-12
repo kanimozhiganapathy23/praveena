69339200aa05f6c5aa380b0c7ae409f3da833dc8
Nagalakshmikesavan <34570264+Nagalakshmikesavan@users.noreply.github.com>
2018-06-12 22:16:32 +1000

    Create sub hrs min.py

---
+++ sub hrs min.py
@@ -0,0 +1,13 @@
+n1=int(input('Enter hour1\n'))
+print(n1)
+n2=int(input('Enter min1\n'))
+print(n2)
+n3=int(input('Enter hour2\n'))
+print(n3)
+n4=int(input('Enter min2\n'))
+print(n4)
+a=n1-n3
+b=n2-n4
+print(str(a)+"."+str(b))
+           
+
